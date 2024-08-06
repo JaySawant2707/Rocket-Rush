2e79ca87b891937b3059d86a79ae23e598abfc3b
Jay Sawant <164754866+JaySawant2707@users.noreply.github.com>
2024-08-06 18:24:04 +0530

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float thrustForce = 1000f;
+    [SerializeField] float RotatingForce = 100f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processThrust();
+        processRotate();
+    }
+
+    void processThrust()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddRelativeForce(Vector3.up * thrustForce * Time.deltaTime);
+        }
+    }
+
+    void processRotate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            applyRotation(RotatingForce);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            applyRotation(-RotatingForce);
+        }
+    }
+
+    void applyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;   //freezing rotations so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;  //Unfreezing rotations so that physics system can take over
+    }
+}
